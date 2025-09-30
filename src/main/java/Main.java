a0dc8ed2096f7de895ca660b3c6eef7f5187a2d4
eriinys <eriinys@duck.com>
2025-09-30 09:51:29 -0500

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        StringBuilder myBuilder = new StringBuilder();
+
+        System.out.println("Please provide the following information:");
+
+
+
+
+
+
+    }
 }
